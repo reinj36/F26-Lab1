d8013f070c7636c69102bbd71c4ca28cd98f52ff
reinj36 <rjames36@myseneca.ca>
2026-09-18 16:59:19 +0000

    Completed Lab1b in class.

--- lab1b.py
+++ lab1b.py
@@ -11,11 +11,25 @@
 #	Create another variable called "num2" and take its value from user. 
 # Convert the values to integers using int() function
 
-num1 = input("Enter a value.")
-num2 = input("Enter a value again.") 
+#get user input
+print()
+num1 = input("Enter a value: ")
+num2 = input("Enter a value again: ") 
 
-num1 = 
+num1 = int(num1)
+num2 = int(num2)
 
 
 # TO-DO 2:
 # Perform all arithmetic oeprations as outlined in the description in README.md file, and print in the required format.
+
+print() #add a space between input and results for formatting
+
+#print results
+print("num1 + num2 = " + str(num1 + num2))
+print("num1 - num2 = " + str(num1 - num2))
+print("num1 * num2 = " + str(num1 * num2))
+print("num1 ** num2 = " + str(num1 ** num2))
+print("num1 / num2 = " + str(num1/num2))
+print("num1 // num2 = " + str(num1//num2))
+print("num1 % num2 = " + str(num1%num2))
